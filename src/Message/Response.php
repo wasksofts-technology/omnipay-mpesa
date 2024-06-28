686f44ebfef7391b9a34520bd4a69a501dff0b75
wasksoftstech <141646352+wasksofts-technology@users.noreply.github.com>
2024-06-28 19:22:33 +0300

    Create Response.php

---
+++ src/Message/Response.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Omnipay\Mpesa\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RequestInterface;
+
+/**
+ * Response
+ */
+class Response extends AbstractResponse
+{
+
+    public function __construct(RequestInterface $request, $data)
+    {
+        $this->request = $request;
+        $this->data = $data;
+    }
+
+    public function isSuccessful()
+    {
+        return isset($this->data['ResponseCode']) && in_array($this->data['ResponseCode'], array('0', '000000'));
+    }
+
+    public function getTransactionReference()
+    {
+        foreach (array(
+            'CheckoutRequestID',
+            'ResultDesc',
+            'MerchantRequestID',
+            'ResponseDescription'
+        ) as $key) {
+            if (isset($this->data[$key])) {
+                return $this->data[$key];
+            }
+        }
+    }
+
+    public function getMessage()
+    {
+        if (isset($this->data['errorCode'])) {
+            return $this->data['errorCode'];
+        }
+
+        if (isset($this->data['errorMessage'])) {
+            return $this->data['errorMessage'];
+        }
+
+        return null;
+    }
+}
