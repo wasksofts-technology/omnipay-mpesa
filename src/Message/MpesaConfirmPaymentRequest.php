f2a62a406f4945fd6e47998d3ef9f28599c8eb13
wasksoftstech <141646352+wasksofts-technology@users.noreply.github.com>
2024-07-12 12:47:51 +0300

    Create MpesaConfirmPaymentRequest.php

---
+++ src/Message/MpesaConfirmPaymentRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Omnipay\Mpesa\Message;
+
+/**
+ * Authorize Request
+ *
+ * @method Response send()
+ */
+class MpesaConfirmPaymentRequest extends AbstractRequest
+{
+    public function getData()
+    {
+        $this->validate(
+            'pt_number',
+            'password',
+            'timestamp',
+            'checkout_request_id',
+
+        );
+
+        $timestamp = date('YmdHis');
+        $data['BusinessShortCode'] = $this->getPTNumber();
+        $data['Password'] = $this->generatePassword($timestamp);
+        $data['Timestamp'] =  $timestamp;
+        $data['CheckoutRequestID'] = $this->getCheckoutRequestID();
+    }
+
+    public function getCheckoutRequestID()
+    {
+        return $this->getParameter('checkout_request_id');
+    }
+
+    public function setCheckoutRequestID($value)
+    {
+        return $this->setParameter('checkout_request_id', $value);
+    }
+
+    public function generatePassword($timesatamp)
+    {
+        return  base64_encode($this->getStorenumber() . $this->getPassKey() . $timesatamp);
+    }
+}
