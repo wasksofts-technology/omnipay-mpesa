92d68538012f29f248269296616404337df98029
wasksoftstech <141646352+wasksofts-technology@users.noreply.github.com>
2025-01-12 12:39:52 +0300

    Update AuthorizeRequest.php

--- src/Message/AuthorizeRequest.php
+++ src/Message/AuthorizeRequest.php
@@ -2,174 +2,81 @@
 
 namespace Omnipay\Mpesa\Message;
 
-use Omnipay\Common\Exception\InvalidResponseException;
-use Omnipay\Common\Message\AbstractRequest as BaseAbstractRequest;
-
 /**
- * Abstract Request
+ * Authorize Request
  *
+ * @method Response send()
  */
-abstract class AbstractRequest extends BaseAbstractRequest
+class AuthorizeRequest extends AbstractRequest
 {
-    /**
-     * Live Endpoint URL
-     *
-     * When you’re set to go live, use the live credentials assigned to
-     * your app to generate a new access token to be used with the live URIs.
-     *
-     * @var string URL
-     */
-    protected $liveEndpoint = 'https://api.safaricom.co.ke/';
-
-    /**
-     * Sandbox Endpoint URL
-     *
-     * The mpesa are supported in two environments. Use the Sandbox environment
-     * for testing purposes, then move to the live environment for production processing.
-     * When testing, generate an access token with your test credentials to make calls to
-     * the Sandbox URIs. When you’re set to go live, use the live credentials assigned to
-     * your app to generate a new access token to be used with the live URIs.
-     *
-     * @var string URL
-     */
-    protected $testEndpoint = 'https://sandbox.safaricom.co.ke/';
-
-    public function getTestMode()
+    public function getData()
     {
-        return $this->getParameter('testMode');
+        $this->validate(
+            'amount',
+            'description',
+            'callbackUrl',
+            'account',
+            'phone_number'
+        );
+
+        $timestamp = date('YmdHis');
+        $data['BusinessShortCode'] = $this->getStoreNumber();
+        $data['Password'] = $this->generatePassword($timestamp);
+        $data['Timestamp'] =  $timestamp;
+        $data['TransactionType'] = $this->getTransactionType();
+        $data['CallBackURL'] = $this->getCallBackUrl();
+        $data['Amount'] = $this->getAmount();
+        $data['PartyA'] = $this->getPhoneNumber();
+        $data['PartyB'] = $this->getPTNumber();
+        $data['PhoneNumber'] =  $this->getPhoneNumber();
+        $data['AccountReference'] =  $this->getAccount();
+        $data['TransactionDesc'] =  $this->getDescription();
+
+        return $data;
     }
 
-    public function setTestMode($value)
+    public function getAmount()
     {
-        return $this->setParameter('testMode', $value);
+        return $this->getParameter('amount');
     }
 
-    public function getTransactionType()
+    public function setAmount($value)
     {
-        return $this->getParameter('transaction_type');
+        return $this->setParameter('amount', $value);
     }
 
-    public function setTransactionType($value)
+    public function getAccount()
     {
-        return $this->setParameter('transaction_type', $value);
+        return $this->getParameter('account');
     }
 
-
-    public function getConsumerKey()
+    public function setAccount($value)
     {
-        return $this->getParameter('consumer_key');
+        return $this->setParameter('account', $value);
     }
 
-    public function setConsumerKey($value)
+    public function getPhoneNumber()
     {
-        return $this->setParameter('consumer_key', $value);
+        return $this->getParameter('phone_number');
     }
 
-    public function getConsumerSecret()
+    public function setPhoneNumber($value)
     {
-        return $this->getParameter('consumer_secret');
+        return $this->setParameter('phone_number', $value);
     }
 
-    public function setConsumerSecret($value)
+    public function getCallBackUrl()
     {
-        return $this->setParameter('consumer_secret', $value);
+        return $this->getParameter('callbackUrl');
     }
 
-    public function getToken()
+    public function setCallBackUrl($value)
     {
-        return $this->getParameter('token');
+        return $this->setParameter('callbackUrl', $value);
     }
 
-    public function setToken($value)
+    public function generatePassword($timesatamp)
     {
-        return $this->setParameter('token', $value);
-    }
-
-    public function getStoreNumber()
-    {
-        return $this->getParameter('storenumber');
-    }
-
-    public function setStoreNumber($value)
-    {
-        return $this->setParameter('storenumber', $value);
-    }
-
-    public function getPTNumber()
-    {
-        return $this->getParameter('pt_number');
-    }
-
-    public function setPTNumber($value)
-    {
-        return $this->setParameter('pt_number', $value);
-    }
-
-    public function getPassKey()
-    {
-        return $this->getParameter('passkey');
-    }
-
-    public function setPassKey($value)
-    {
-        return $this->setParameter('passkey', $value);
-    }
-
-    protected function getEndpoint()
-    {
-        return $this->getTestMode() == false ? $this->liveEndpoint : $this->testEndpoint;
-    }
-
-
-    public function sendData($data)
-    {
-        // Might be useful to have some debug code here, PayPal especially can be
-        // a bit fussy about data formats and ordering.  Perhaps hook to whatever
-        // logging engine is being used.
-        $body = $this->toJSON($data);
-        try {
-            $httpResponse = $this->httpClient->request(
-                'POST',
-                $this->getEndpoint() . 'mpesa/stkpush/v1/processrequest',
-                [
-                    'Accept' => 'application/json',
-                    'Authorization' => 'Bearer ' . $this->getToken(),
-                    'Content-Type' => 'application/json',
-                ],
-                $body
-            );
-            // Empty response body should be parsed also as and empty array
-            $body = (string) $httpResponse->getBody()->getContents();
-            $jsonToArrayResponse = !empty($body) ? json_decode($body, true) : array();
-            return $this->response = $this->createResponse($jsonToArrayResponse, $httpResponse->getStatusCode());
-        } catch (\Exception $e) {
-            throw new InvalidResponseException(
-                'Error communicating with payment gateway: ' . $e->getMessage(),
-                $e->getCode()
-            );
-        }
-    }
-
-    /**
-     * Returns object JSON representation required by PayPal.
-     * The PayPal REST API requires the use of JSON_UNESCAPED_SLASHES.
-     *
-     * @param int $options http://php.net/manual/en/json.constants.php
-     * @return string
-     */
-    public function toJSON($data, $options = 0)
-    {
-        // Because of PHP Version 5.3, we cannot use JSON_UNESCAPED_SLASHES option
-        // Instead we would use the str_replace command for now.
-        // TODO: Replace this code with return json_encode($this->toArray(), $options | 64); once we support PHP >= 5.4
-        if (version_compare(phpversion(), '5.4.0', '>=') === true) {
-            return json_encode($data, $options | 64);
-        }
-        return str_replace('\\/', '/', json_encode($data, $options));
-    }
-
-    protected function createResponse($data, $statusCode)
-    {
-        return $this->response = new Response($this, $data, $statusCode);
+        return  base64_encode($this->getStorenumber() . $this->getPassKey() . $timesatamp);
     }
 }
